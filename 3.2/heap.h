e739fe8f2f0f31100712e1c67ad26528572859dc
A01352038 <112148764+A01352038@users.noreply.github.com>
2022-12-03 22:03:03 -0600

    Add files via upload

---
+++ 3.2/heap.h
@@ -0,0 +1,52 @@
+// =================================================================
+//
+// File: heap.h
+// Author: Sergio Augusto Macías Corona
+// Date:
+//
+// =================================================================
+#ifndef HEAP_H
+#define HEAP_H
+
+#include <iostream>
+#include <string>
+#include <sstream>
+#include <vector>
+#include <climits>
+
+template <class T>
+class Heap
+{
+public:
+    // Constructor
+    Heap() = default;
+    Heap(const Heap<T> &); // copy constructor
+
+    // Destructor
+    ~Heap() = default;
+
+    // Access Methods
+    T get_size() const;
+    T get_top() const;
+
+    // Functions
+    bool is_empty() const;
+    void push(T);
+    void pop();
+
+    std::string heap_elements() const;
+
+private:
+    std::vector<T> elements;
+
+    // Access Method
+    unsigned int get_parent(unsigned int) const;
+    unsigned int get_left_leaf(unsigned int) const;
+    unsigned int get_right_leaf(unsigned int) const;
+
+    // Functions
+    void swap(unsigned int, unsigned int);
+    void push_up(unsigned int);
+};
+
+#endif //! HEAP_H
